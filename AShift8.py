fe7f5a0a6595ad0899d6e7b413249fe3904ecf63
piotrost <piotrostaszewski412@gmail.com>
2024-11-07 18:06:25 +0100

    przejscie na gdb

--- AShift8.py
+++ AShift8.py
@@ -10,14 +10,14 @@ class Node:
     def __init__(self):                             # no need to store node's coordinates - they are keys in "nodes" dictionary
         self.edges = []                             # keys to nodes in the "nodes dictionary" + edge info
 
-    def add_edge(self, x, y, length, fid):
-        self.edges.append(Edge(x, y, length, fid))
+    def add_edge(self, x, y, length, OBJECTID):
+        self.edges.append(Edge(x, y, length, OBJECTID))
 
 class Edge:
-    def __init__(self, x, y, length, fid):
+    def __init__(self, x, y, length, OBJECTID):
         self.xy = (x, y)
         self.length = length
-        self.fid = fid                              # for visualization?
+        self.OBJECTID = OBJECTID                              # for visualization?
     
     # h function
     def h(self, end):                                    
@@ -26,11 +26,11 @@ class Edge:
 class Graph:
     def __init__(self, file):
         self.file = None                            # graph data source
-        self.nodes = {}                             # all read data
+        self.nodes = {}                             # the nodes - core data structure for A*
         self.read_graph(file)
     
     def read_graph(self, file):
-        with arcpy.da.SearchCursor(file, ['FID', 'SHAPE@']) as cursor:
+        with arcpy.da.SearchCursor(file, ['OBJECTID', 'SHAPE@']) as cursor:
             self.file = file                        # save graph data source path
 
             temp_nodes = {}                         # store the nodes' final rounded coordinates under multiple keys
@@ -38,7 +38,7 @@ class Graph:
                 shape = row[1]                      # only to get nodes' coordinates
                 first_point = shape.firstPoint      # node
                 last_point = shape.lastPoint        # second node
-                fid = row[0]                        # edge fid
+                OBJECTID = row[0]                        # edge OBJECTID
                 length = shape.length               # edge length
 
                 xy_arr = []                         # final nodes' coordinates
@@ -70,16 +70,16 @@ class Graph:
                     xy_arr.append(xyf)
                 
                 # create edges - in two directions
-                self.nodes[xy_arr[0]].add_edge(xy_arr[1][0], xy_arr[1][1], length, fid)
-                self.nodes[xy_arr[1]].add_edge(xy_arr[0][0], xy_arr[0][1], length, fid)
+                self.nodes[xy_arr[0]].add_edge(xy_arr[1][0], xy_arr[1][1], length, OBJECTID)
+                self.nodes[xy_arr[1]].add_edge(xy_arr[0][0], xy_arr[0][1], length, OBJECTID)
     
     def export_graph_txt(self):
         with open("my_graph.txt", "w") as f:
             for node in self.nodes:
                 f.write(f"\n\n\t<-- {node} -->\n")
                 for edge in self.nodes[node].edges:
-                    f.write(f"{edge.xy}\t{edge.length}\t{edge.fid}\n")
-    
+                    f.write(f"{edge.xy}\t{edge.length}\t{edge.OBJECTID}\n")
+
     def aShift8(self, start, end):
         # create data structures
         S = set(); S.add(start)
@@ -90,7 +90,7 @@ class Graph:
         for edge in self.nodes[start].edges:
             future_h = edge.h(end)
             Q[edge.xy] = edge.length + future_h, edge.length, future_h  # f, g, h for nodes added to Q - possible FUTURE S elements
-            p[edge.xy] = start, edge.fid
+            p[edge.xy] = start, edge.OBJECTID
         
         # main algorithm loop
         while Q:                                                        # ensure loop exit when no solution
@@ -98,15 +98,15 @@ class Graph:
             
             # destination reached
             if curr == end:
-                node_path = [end]; fids = []            # create output data structures
+                node_path = [end]; OBJECTIDs = []            # create output data structures
                 curr = (end, None)                      # adjust curr for loop
                 while curr[0] != start:
                     curr = p[curr[0]]                   # alter curr
                     node_path.append(curr[0])           # append node
-                    fids.append(curr[1])                # append edge
+                    OBJECTIDs.append(curr[1])                # append edge
                 
-                node_path.reverse(); fids.reverse()
-                return node_path, fids, curr_g, len(S)
+                node_path.reverse(); OBJECTIDs.reverse()
+                return node_path, OBJECTIDs, curr_g, len(S)
             
             # add the current node to the S set
             S.add(curr)
@@ -117,7 +117,7 @@ class Graph:
                         future_h = edge.h(end)                         # h for new node - it will never be changed
                         future_g = curr_g + edge.length                         # g for new node                      
                         Q[edge.xy] = future_g + future_h, future_g, future_h    # f, g, h
-                        p[edge.xy] = curr, edge.fid
+                        p[edge.xy] = curr, edge.OBJECTID
                     else:
                         new_old_h = Q[edge.xy][2]                               # read Q[edge.xy][2] = old_but_up_to_date_h
                         new_g = curr_g + edge.length                            # count possibly_different_value_of_g
@@ -125,73 +125,65 @@ class Graph:
                         # relax the edge (if needed)
                         if new_f < Q[edge.xy][0]:                               # Q[edge.xy][0] = old_f
                             Q[edge.xy] = new_f, new_g, new_old_h                # f, g, h
-                            p[edge.xy] = curr, edge.fid
+                            p[edge.xy] = curr, edge.OBJECTID
+
+    def export_fc(self):
+            # create output feature class in gdb
+            arcpy.management.CreateFeatureclass(arcpy.env.workspace, 'output', 'POLYLINE')
+            
+            # new fields
+            field_names = ["EDGE_ID","F_POINT", "L_POINT"]
+            field_types = ["INTEGER", "TEXT", "TEXT"]
+
+            # add the fields to feature class
+            for field_name, field_type in zip(field_names, field_types):
+                arcpy.AddField_management('output', field_name, field_type)
+
+            # copying
+            with arcpy.da.SearchCursor(self.file, ["OBJECTID", "SHAPE@"]) as cursor:
+                with arcpy.da.InsertCursor(arcpy.env.workspace + r'\output', ["EDGE_ID","F_POINT", "L_POINT", "SHAPE@"]) as insert_cursor:
+                    for row in cursor:
+                        if row[0] in OBJECTIDs:
+                            OBJECTID = row[0]
+                            shape = row[1]
+                            first_point = shape.firstPoint
+                            first_point = "(" + str(first_point.X) + ", " + str(first_point.Y) + ")"
+                            last_point = shape.lastPoint
+                            last_point = "(" + str(last_point.X) + ", " + str(last_point.Y) + ")"
+
+                            insert_cursor.insertRow([OBJECTID, first_point, last_point, shape])
+
+            print("Creating an output feature class completed.")
 
 if __name__ == '__main__':
-    import time
-
     curr_directory = os.getcwd()
 
-    # The test
-    t_1 = time.time()
-    g = Graph(curr_directory + r'\data\L4_1_BDOT10k__OT_SKJZ_L.shp')
-    t0 = time.time()
-    path, fids, length, vol_s = g.aShift8((471892, 576471),(481676, 574633))
-    t1 = time.time()
-    print(t1)
+    # testing
+    import time
 
+    # arcpy gdb settings
+    arcpy.env.workspace = curr_directory + r"\workspace.gdb"
+    arcpy.env.overwriteOutput = True
+    arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("ETRF2000-PL CS92")
+
+    # reading
+    t_1 = time.time()
+    g = Graph('SKJZ_L_Torun_m')
+    
+    # algorithm
+    t0 = time.time()
+    path, OBJECTIDs, length, vol_s = g.aShift8((471892, 576471),(481676, 574633))
+    t1 = time.time()
+
+    # printing
     print("reading", t0-t_1, "s")
     print("algorithm:", t1-t0, "s")
     
-    # printing
     g.export_graph_txt()
     print( "volume of S:        ", vol_s)
     print("length of the road: ", length)
     print('path vertices count:', len(path))
-    print('path edges count:   ', len(fids))
-
-    # The not-a-real-visualization ############################################
-    # output folder
-    output_folder = curr_directory + r'\output'
-    for filename in os.listdir(output_folder):
-        file_path = os.path.join(output_folder, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+    print('path edges count:   ', len(OBJECTIDs))
     
-    # input shp
-    input_shp = g.file
-
-    # output shp
-    output_shp = output_folder + r'\output.shp'
-
-    # the same geometry and crs as the input
-    arcpy.CreateFeatureclass_management(
-        os.path.dirname(output_shp),
-        os.path.basename(output_shp),
-        arcpy.Describe(input_shp).shapeType,
-        spatial_reference=arcpy.Describe(input_shp).spatialReference
-    )
-
-    # new fields
-    field_names = ["EDGE_FID","F_POINT", "L_POINT"]
-    field_types = ["INTEGER", "TEXT", "TEXT"]
-
-    # add the fields to shp
-    for field_name, field_type in zip(field_names, field_types):
-        arcpy.AddField_management(output_shp, field_name, field_type)
-
-    # copying
-    with arcpy.da.SearchCursor(input_shp, ["FID", "SHAPE@"]) as cursor:
-        with arcpy.da.InsertCursor(output_shp, ["EDGE_FID","F_POINT", "L_POINT", "SHAPE@"]) as insert_cursor:
-            for row in cursor:
-                if row[0] in fids:
-                    fid = row[0]
-                    shape = row[1]
-                    first_point = shape.firstPoint
-                    first_point = "(" + str(first_point.X) + ", " + str(first_point.Y) + ")"
-                    last_point = shape.lastPoint
-                    last_point = "(" + str(last_point.X) + ", " + str(last_point.Y) + ")"
-
-                    insert_cursor.insertRow([fid, first_point, last_point, shape])
-
-    print("Creating a stupid shp instead of a proper visualization completed.")
+    # visualization
+    g.export_fc()
